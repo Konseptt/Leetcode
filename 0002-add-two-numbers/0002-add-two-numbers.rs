c4d951cb3832f88f39e71e8fa4779087b1f96bef
Konseptt <sharmaranjan816@gmail.com>
2024-08-26 08:35:51 +0530

    Time: 3 ms (65.62%), Space: 2.2 MB (73.22%) - LeetHub

---
+++ 0002-add-two-numbers/0002-add-two-numbers.rs
@@ -0,0 +1,24 @@
+impl Solution {
+    pub fn add_two_numbers(
+        mut l1: Option<Box<ListNode>>,
+        mut l2: Option<Box<ListNode>>,
+    ) -> Option<Box<ListNode>> {
+        let mut dummy = Some(Box::new(ListNode::new(0)));
+        let mut cur = &mut dummy;
+        let mut sum = 0;
+        while l1.is_some() || l2.is_some() || sum != 0 {
+            if let Some(node) = l1 {
+                sum += node.val;
+                l1 = node.next;
+            }
+            if let Some(node) = l2 {
+                sum += node.val;
+                l2 = node.next;
+            }
+            cur.as_mut().unwrap().next = Some(Box::new(ListNode::new(sum % 10)));
+            cur = &mut cur.as_mut().unwrap().next;
+            sum /= 10;
+        }
+        dummy.unwrap().next.take()
+    }
+}
