8f3d5484df7f8855b7dd0f13ab574d5dde4fd6dc
Konseptt <sharmaranjan816@gmail.com>
2024-11-02 10:20:36 +0530

    Time: 7 ms (100.00%), Space: 2.6 MB (31.03%) - LeetHub

--- 1957-delete-characters-to-make-fancy-string/1957-delete-characters-to-make-fancy-string.rs
+++ 1957-delete-characters-to-make-fancy-string/1957-delete-characters-to-make-fancy-string.rs
@@ -1,15 +1,17 @@
 impl Solution {
-    pub fn make_fancy_string(s: String) -> String {
-        let mut s = s.into_bytes();
-        let mut write = 0;
-        for read in 0..s.len() {
-            if write >= 2 && s[read] == s[write - 1] && s[read] == s[write - 2] {
-                continue;
+    pub fn make_fancy_string(mut s: String) -> String {
+        unsafe {
+            let bytes = s.as_bytes_mut();
+            let mut write = 0;
+            for read in 0..bytes.len() {
+                if write >= 2 && bytes[read] == bytes[write - 1] && bytes[read] == bytes[write - 2] {
+                    continue;
+                }
+                bytes[write] = bytes[read];
+                write += 1;
             }
-            s[write] = s[read];
-            write += 1;
+            s.truncate(write);
         }
-        s.truncate(write);
-        unsafe { String::from_utf8_unchecked(s) }
+        s
     }
 }
