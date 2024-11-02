86b965d15cbca7f5299acc5c9309b0efebbd5f28
Konseptt <sharmaranjan816@gmail.com>
2024-11-02 10:19:21 +0530

    Time: 0 ms (100.00%), Space: 2.5 MB (75.86%) - LeetHub

--- 1957-delete-characters-to-make-fancy-string/1957-delete-characters-to-make-fancy-string.rs
+++ 1957-delete-characters-to-make-fancy-string/1957-delete-characters-to-make-fancy-string.rs
@@ -1,15 +1,15 @@
 impl Solution {
     pub fn make_fancy_string(s: String) -> String {
-        let mut ans = String::with_capacity(s.len());
-        let mut prev1 = '\0';
-        let mut prev2 = '\0';
-        for c in s.chars() {
-            if c != prev1 || c != prev2 {
-                ans.push(c);
-                prev2 = prev1;
-                prev1 = c;
+        let mut s = s.into_bytes();
+        let mut write = 0;
+        for read in 0..s.len() {
+            if write >= 2 && s[read] == s[write - 1] && s[read] == s[write - 2] {
+                continue;
             }
+            s[write] = s[read];
+            write += 1;
         }
-        ans
+        s.truncate(write);
+        unsafe { String::from_utf8_unchecked(s) }
     }
 }
