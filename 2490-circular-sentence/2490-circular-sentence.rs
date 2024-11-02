fdee1b8a6850b50229cb8b9a46aa663a9ea81a3e
Konseptt <sharmaranjan816@gmail.com>
2024-11-02 10:14:33 +0530

    Time: 0 ms (100.00%), Space: 2.3 MB (50.00%) - LeetHub

--- 2490-circular-sentence/2490-circular-sentence.rs
+++ 2490-circular-sentence/2490-circular-sentence.rs
@@ -1,12 +1,18 @@
 impl Solution {
     pub fn is_circular_sentence(sentence: String) -> bool {
-        let ss: Vec<String> = sentence.split(' ').map(String::from).collect();
-        let n = ss.len();
-        for i in 0..n {
-            if ss[i].as_bytes()[ss[i].len() - 1] != ss[(i + 1) % n].as_bytes()[0] {
+        let n = sentence.len();
+        let chars: Vec<char> = sentence.chars().collect();
+
+        if chars[0] != chars[n - 1] {
+            return false;
+        }
+
+        for i in 1..n - 1 {
+            if chars[i] == ' ' && chars[i - 1] != chars[i + 1] {
                 return false;
             }
         }
-        return true;
+
+        true
     }
 }
