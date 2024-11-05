41420f0b38e0281765a189713959f1708f9ed6c9
Konseptt <sharmaranjan816@gmail.com>
2024-11-05 07:22:52 +0530

    Time: 0 ms (100.00%), Space: 2.2 MB (83.33%) - LeetHub

--- 2914-minimum-number-of-changes-to-make-binary-string-beautiful/2914-minimum-number-of-changes-to-make-binary-string-beautiful.rs
+++ 2914-minimum-number-of-changes-to-make-binary-string-beautiful/2914-minimum-number-of-changes-to-make-binary-string-beautiful.rs
@@ -1,9 +1,8 @@
 impl Solution {
     pub fn min_changes(s: String) -> i32 {
         let mut ans = 0;
-        let bytes = s.as_bytes();
-        for i in (1..bytes.len()).step_by(2) {
-            if bytes[i] != bytes[i - 1] {
+        for i in (1..s.len()).step_by(2) {
+            if s.as_bytes()[i] != s.as_bytes()[i - 1] {
                 ans += 1;
             }
         }
