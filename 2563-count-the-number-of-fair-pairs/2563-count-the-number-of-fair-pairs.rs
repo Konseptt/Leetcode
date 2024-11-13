55187dc6f8a37f9c940b39aa6625a0f75dd96cfe
Konseptt <sharmaranjan816@gmail.com>
2024-11-13 15:46:09 +0530

    Time: 24 ms (100.00%), Space: 3.5 MB (100.00%) - LeetHub

--- 2563-count-the-number-of-fair-pairs/2563-count-the-number-of-fair-pairs.rs
+++ 2563-count-the-number-of-fair-pairs/2563-count-the-number-of-fair-pairs.rs
@@ -1,13 +1,13 @@
 impl Solution {
     pub fn count_fair_pairs(nums: Vec<i32>, lower: i32, upper: i32) -> i64 {
         let mut nums = nums;
-        nums.sort();
+        nums.sort_unstable();
         let mut ans = 0;
 
         for i in 0..nums.len() {
             let x = nums[i];
-            let lower_bound = nums[(i + 1)..].partition_point(|&y| y < lower - x) + i + 1;
-            let upper_bound = nums[(i + 1)..].partition_point(|&y| y <= upper - x) + i + 1;
+            let lower_bound = (i + 1) + nums[i + 1..].partition_point(|&y| y < lower - x);
+            let upper_bound = (i + 1) + nums[i + 1..].partition_point(|&y| y <= upper - x);
             ans += (upper_bound - lower_bound) as i64;
         }
 
