91daf76ff544f3159a0d430981086f5a01e242f9
Konseptt <sharmaranjan816@gmail.com>
2024-11-16 09:45:36 +0530

    Time: 0 ms (100.00%), Space: 2.1 MB (87.50%) - LeetHub

--- 3254-find-the-power-of-k-size-subarrays-i/3254-find-the-power-of-k-size-subarrays-i.rs
+++ 3254-find-the-power-of-k-size-subarrays-i/3254-find-the-power-of-k-size-subarrays-i.rs
@@ -7,13 +7,13 @@ impl Solution {
         }
         let mut ans = Vec::with_capacity(n.saturating_sub(k - 1));
         let mut streak = 1;
-
         for i in 0..n {
             if i > 0 && nums[i] == nums[i - 1] + 1 {
                 streak += 1;
             } else {
                 streak = 1;
             }
+
             if i + 1 >= k {
                 if streak >= k {
                     ans.push(nums[i]);
