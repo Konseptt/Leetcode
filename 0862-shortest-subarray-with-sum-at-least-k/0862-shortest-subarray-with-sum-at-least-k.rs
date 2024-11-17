ff046a9f86401da3ab910a6137bc6c5243564153
Konseptt <sharmaranjan816@gmail.com>
2024-11-17 17:14:31 +0530

    Time: 18 ms (37.50%), Space: 3.7 MB (100.00%) - LeetHub

--- 0862-shortest-subarray-with-sum-at-least-k/0862-shortest-subarray-with-sum-at-least-k.rs
+++ 0862-shortest-subarray-with-sum-at-least-k/0862-shortest-subarray-with-sum-at-least-k.rs
@@ -1,42 +1,37 @@
 use std::cmp::min;
-use std::collections::VecDeque;
+
 
 impl Solution {
     pub fn shortest_subarray(nums: Vec<i32>, k: i32) -> i32 {
         let n = nums.len();
-        let mut prefix_sums = vec![0i64; n + 1];
-        for i in 0..n {
-            prefix_sums[i + 1] = prefix_sums[i] + nums[i] as i64;
+        let k = k as i64;
+        let mut prefix_sums = Vec::with_capacity(n + 1);
+        prefix_sums.push(0i64);
+        for &num in &nums {
+            prefix_sums.push(prefix_sums[prefix_sums.len() - 1] + num as i64);
         }
 
-        let mut deque: VecDeque<usize> = VecDeque::new();
+        let mut deque = Vec::with_capacity(n + 1);
+        let mut head = 0;
         let mut min_length = n + 1;
 
         for i in 0..=n {
-            while let Some(&front) = deque.front() {
-                if prefix_sums[i] - prefix_sums[front] >= k as i64 {
-                    min_length = min(min_length, i - front);
-                    deque.pop_front();
-                } else {
-                    break;
-                }
+            while head < deque.len() && prefix_sums[i] - prefix_sums[deque[head]] >= k {
+                min_length = min(min_length, i - deque[head]);
+                head += 1;
             }
 
-            while let Some(&back) = deque.back() {
-                if prefix_sums[back] >= prefix_sums[i] {
-                    deque.pop_back();
-                } else {
-                    break;
-                }
+            while deque.len() > head && prefix_sums[i] <= prefix_sums[*deque.last().unwrap()] {
+                deque.pop();
             }
 
-            deque.push_back(i);
+            deque.push(i);
         }
 
-        if min_length > n {
-            -1
-        } else {
+        if min_length <= n {
             min_length as i32
+        } else {
+            -1
         }
     }
 }
